95071bd245b63438cafdb9d9ea5932c2071428ef
kimyd34 <tommykimyd98@gmail.com>
2021-06-16 13:19:01 +0900

    layoutinflater를 이용한 toast모양 변경 실습

--- app/src/main/java/com/example/android_event_study_ex/MainActivity.java
+++ app/src/main/java/com/example/android_event_study_ex/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
@@ -12,39 +13,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toast toast;
+    View layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editText = findViewById(R.id.edittextView);
-        View view = findViewById(R.id.View);
-        View view2 = findViewById(R.id.View2);
 
-        view.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                int a = event.getAction();
+            layout = getLayoutInflater().inflate(R.layout.toast_layout,findViewById(R.id.toast_layout));
+            toast = Toast.makeText(this,"토스트가 생성 되었습니다.", Toast.LENGTH_SHORT);
+            toast.setView(layout);
 
-                float x = event.getX();
-                float y = event.getY();
-
-                if(a == MotionEvent.ACTION_DOWN){
-                    editText.append(x + "," + y);
-                    Toast.makeText(getBaseContext(),editText.getText().toString(), Toast.LENGTH_LONG).show();
-                }
-                else if(a == MotionEvent.ACTION_MOVE){
-                    editText.append(x + "," + y);
-                }
-                return false;
-            }
-        });
     }
 
-    @Override
-    protected void onSaveInstanceState( Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        outState.putAll(outState);
+    public void button_clicked(View view) {
+        toast.show();
     }
-
 }
